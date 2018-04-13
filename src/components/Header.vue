3fcf549876c19a4f3b9010b4beade4fbfbc5a3f5
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 16:18:51 +0200

    141 finishing touches and state management

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+      <div class="col-sm-12">
+          <h2>Quotes Added</h2>
+          <div class="progress-bar" role="progressbar" 
+            aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+            :style="{width: ((quotesCount / maxQuotes) * 100) + '%'}">
+                {{ quotesCount }} / {{ maxQuotes }}
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["quotesCount", "maxQuotes"]
+};
+</script>
+
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row">
+        
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label for="#quote">Quote</label>
+                <textarea name="quote" id="quote" v-model="quote" rows="3" class="form-control"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+
+export default {
+  data: function() {
+    return { quote: "" };
+  },
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    createNew() {
+        this.$emit('quoteAdded', this.quote);
+        this.quote = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
