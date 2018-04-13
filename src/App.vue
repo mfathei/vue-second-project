bb39f1721b97604c9a8633905ada6e372324f664
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 15:47:25 +0200

    139 allowing for quote deletion

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
       <app-new-quote @quoteAdded="createNewQuote"></app-new-quote>
-      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
       <div class="row">
         <div class="col-xs-12 text-center">
           <div class="alert alert-info">Info: click on Quote to delete it!</div>
@@ -24,6 +24,9 @@ export default {
   methods: {
     createNewQuote(quote) {
       this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
   },
   components: {
